cf80d39f4f9918bbb1ca50d8da92d9cfdaaf2aa5
kamwanasamwel <kamwanasamwel@gmail.com>
2018-06-15 20:55:16 +0300

    with readme

--- FindById/settings.py
+++ FindById/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'itemsfound',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'FindById.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'findbyid',
+        'USER': 'admin',
+        'PASSWORD': 'findbyidadmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

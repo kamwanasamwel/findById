fc05dc9cf5a7ea20c347d7dc1270d64ef1fd2ff5
ken <ken7kinyua@gmail.com>
2018-06-16 08:33:11 +0300

    have added changes on the template

--- itemsfound/views.py
+++ itemsfound/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class Homepage(View):
+	def get(self,request):
+		return render(request,'Home/index.html')
+
+
